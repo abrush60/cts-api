37b1871e9aadd1e320ad75f15da5965dc3aa4ff9
Andrew Pinckney <andrewpinckney@Andrews-MBP.hsd1.al.comcast.net>
2021-11-21 18:13:09 -0600

    client table

--- api/Data/SaveClient.cs
+++ api/Data/SaveClient.cs
@@ -26,7 +26,7 @@ namespace api.database
             using var con = new MySqlConnection(cs);
             con.Open();
 
-            string stm = @"CREATE TABLE client(email TEXT PRIMARY KEY AUTO_INCREMENT, userName TEXT)";
+            string stm = @"CREATE TABLE client(email TEXT PRIMARY KEY AUTO_INCREMENT, userName TEXT, clientPassword TEXT, phone TEXT)";
 
             using var cmd = new MySqlCommand(stm, con);
             cmd.ExecuteNonQuery();
